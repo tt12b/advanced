60f4ad5e0d409fa66cb94198f9e168fc9f9e9cb5
yeongho-mo <myh@ulalalab.com>
2025-05-29 11:21:20 +0900

    test

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -10,6 +10,7 @@ public class AdvancedApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdvancedApplication.class, args);
         System.out.println("git Test");
+        System.out.println("git Test2");
         System.out.println("Hello World");
 
     }
